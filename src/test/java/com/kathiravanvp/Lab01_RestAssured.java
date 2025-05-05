ff19e8effe6fd8b1ee05ae16f677f9b44555385c
admin <kathir300499@gmail.com>
2025-05-05 21:42:15 +0530

    5th May

---
+++ src/test/java/com/kathiravanvp/Lab01_RestAssured.java
@@ -0,0 +1,14 @@
+package com.kathiravanvp;
+
+import io.restassured.RestAssured;
+
+public class Lab01_RestAssured {
+    public static void main(String[] args) {
+        RestAssured.given()
+                    .baseUri("https://restful-booker.herokuapp.com/ping")
+                .when()
+                    .get()
+                .then()
+                    .log().all().statusCode(201);
+    }
+}
